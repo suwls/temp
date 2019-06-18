fd5be0ce257abd380411e89e76a0404509048f78
suwls <dltnwls8751@gmail.com>
2019-06-18 00:04:54 +0000

    d

--- receiveImage2.py
+++ receiveImage2.py
@@ -1,5 +1,3 @@
-# -*- coding:utf-8 -*-
-
 import socket
 import sys
 import time
@@ -11,7 +9,7 @@ PORT = 2222
 
 #1. open Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print ('Socket created')
 
 #2. bind to a address and port
@@ -27,7 +25,6 @@ print ('Socket bind complete')
 s.listen(10)
 print ('Socket now listening')
 
-it = 2
 #keep talking with the client
 while 1:
     #4. Accept connection
@@ -35,69 +32,89 @@ while 1:
     print ('Connected with ' + addr[0] + ':' + str(addr[1]))
 
     id = conn.recv(255).decode()
-    print('id : ' + id)
+    print('id : *' + id+ '*')
 
     timestamp = conn.recv(26).decode()
-    print('timestamp : ' + timestamp)
+    print('timestamp : *' + timestamp +'*')
 
     result = conn.recv(1).decode()
-    print('result : ' + result)
+    print('result : #' + result + '#')
 
-    if(result == 1):
-        it = 4
-    elif(result == 0):
-        it = 2
-    print("it" + str(it))
-    for i in range(1, it):
+    if(result == '0'):
+        #push immediately
+        for i in range(1, 2):
         #5. Read/Send
-        img = ""
-        len_data = conn.recv(1024).decode()
-        len_data = int(len_data)
-        size = 0
+            img = ""
+            len_data = conn.recv(1024).decode()
+            len_data = int(len_data)
+            size = 0
 
-        print("Data Reading") 
+            print("Data Reading") 
 
-        while 1:
-            data = conn.recv(65536)
-            str_data = data.decode()
-            img += data.decode()
-            size += len(str_data)
-            if size == len_data:
-                print("Finished Data Reading")
-                break
+            while 1:
+                data = conn.recv(65536)
+                str_data = data.decode()
+                img += data.decode()
+                size += len(str_data)
+                if size == len_data:
+                    print("Finished Data Reading")
+                    break
 
-        with open('/root/openface/temp/test_'+str(i)+'.jpeg', 'wb') as f:
-            f.write(base64.b64decode(img))
-        time.sleep(0.1)
+            with open('/root/openface/temp/test_'+str(i)+'.jpeg', 'wb') as f:
+                f.write(base64.b64decode(img))
+            time.sleep(0.1)
+
+    elif(result == '1'):
+        for i in range(1, 2):
+        #5. Read/Send
+            img = ""
+            len_data = conn.recv(1024).decode()
+            len_data = int(len_data)
+            size = 0
+
+            print("Data Reading") 
+
+            while 1:
+                data = conn.recv(65536)
+                str_data = data.decode()
+                img += data.decode()
+                size += len(str_data)
+                if size == len_data:
+                    print("Finished Data Reading")
+                    break
+
+            with open('/root/openface/temp/test_'+str(i)+'.jpeg', 'wb') as f:
+                f.write(base64.b64decode(img))
+            time.sleep(0.1)
     break
 
 res_list = {}
 
-print("out it : " + str(it))
-for j in range(1, it):
+if(result == '1'):
+    for j in range(1,2):
     # start face recognigionc
-    res=subprocess.check_output(['/root/openface/demos/classifier_test.py infer /root/openface/embedding/us/classifier.pkl /root/openface/temp/test_'+str(j)+'.jpeg'], universal_newlines=True,shell=True)
-    print("res: {}".format(res))
+        res=subprocess.check_output(['/root/openface/demos/classifier_test.py infer /root/openface/users/'+id+'/embedding/classifier.pkl /root/openface/temp/test_'+str(j)+'.jpeg'], universal_newlines=True,shell=True)
+        print("res: {}".format(res))
     
     # store recognition result
-    name, accuracy = res.split(':')
-    accuracy = float(accuracy)
+        name, accuracy = res.split(':')
+        accuracy = float(accuracy)
 
-    if accuracy > 0.5:
-        if name in res_list.keys():
-            res_list[name][1] += 1
-            if res_list[name][0] < accuracy:
-            	res_list[name][0] = accuracy
-        else:
-            res_list[name] = [accuracy, 1]
+        if accuracy > 0.89:
+            if name in res_list.keys():
+                res_list[name][1] += 1
+                if res_list[name][0] < accuracy:
+            	    res_list[name][0] = accuracy
+            else:
+                res_list[name] = [accuracy, 1]
 
-    print("res_list: {}".format(res_list))
+        print("res_list: {}".format(res_list))
 
 if not res_list:
-    print("stranger")
+    print("*stranger*")
 else:
     res_list = sorted(res_list.items(), key = lambda x: (x[1][1], x[1][0]), reverse = True)
-    print("final res: {}".format(res_list[0][0]))
+    print("final res: *{}*".format(res_list[0][0]))
 
 conn.close()
 s.close()
